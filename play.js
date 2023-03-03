566efa03e5d92de50619317083fba6494595c92d
Jaden <jespotten@hotmail.com>
2023-03-03 16:33:22 -0700

    saveScore, updateScores, and buttonDance

--- play.js
+++ play.js
@@ -133,6 +133,60 @@ class Game
             await delay(100);
         }
     }
+
+    saveScore(score)
+    {
+        const userName = this.getPlayerName();
+        let scores = [];
+        const scoresText = localStorage.getItem('scores');
+        if(scoresText)
+        {
+            scores = JSON.parse(scoresText);
+        }
+        scores = this.updateScores(userName, score, scores);
+
+        localStorage.setItem('scores', JSON.stringify(scores));
+    }
+
+    updateScores(userName, score, scores)
+    {
+        const date = new Date().toLocaleDateString();
+        const newScore = {name: userName, score: score, date: date};
+
+        let found = false;
+        for(const [i, prevScore] of scores.entries())
+        {
+            if(score > prevScore.score)
+            {
+                scores.splice(i, 0, newScore);
+                found = true;
+                break;
+            }
+        }
+
+        if(!found)
+        {
+            scores.push(newScore);
+        }
+
+        if(scores.length > 10)
+        {
+            scores.length = 10;
+        }
+
+        return scores;
+    }
+
+    async buttonDance(laps = 1)
+    {
+        for(let step = 0; step < laps; step++)
+        {
+            for(const btn of this.buttons.values())
+            {
+                await btn.press(0.0);
+            }
+        }
+    }
 }
 
 const game = new Game();
